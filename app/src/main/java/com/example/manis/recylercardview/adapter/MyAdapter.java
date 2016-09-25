a713f9545d77297b85cc8bc3dceafcbf6d1ba769
manish <manishmaharzan@gmail.com>
2016-09-25 12:25:35 +0545

    add click response to cardview on recyclerAdapter insdie onBindViewHolder card.setOnclickListener

--- app/src/main/java/com/example/manis/recylercardview/adapter/MyAdapter.java
+++ app/src/main/java/com/example/manis/recylercardview/adapter/MyAdapter.java
@@ -1,11 +1,14 @@
 package com.example.manis.recylercardview.adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.manis.recylercardview.R;
 
@@ -25,7 +28,15 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+
         holder.textView.setText(listItem[position]);
+        holder.card.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(),"hello you just click me",Toast.LENGTH_LONG).show();
+                Log.v("mylog", "Clicked");
+            }
+        });
 
     }
 
@@ -36,13 +47,16 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
+
         TextView textView;
         ImageView imageView;
+        CardView card;
 
         public ViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.title);
             imageView = (ImageView) itemView.findViewById(R.id.img_crd);
+            card= (CardView) itemView;
         }
     }
 }
