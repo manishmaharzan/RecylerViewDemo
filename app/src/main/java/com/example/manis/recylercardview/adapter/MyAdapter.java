d31375301604610509b6306985dd5e56fea2980e
manish <manishmaharzan@gmail.com>
2016-09-25 10:01:55 +0545

    CardView added with static image icon

--- app/src/main/java/com/example/manis/recylercardview/adapter/MyAdapter.java
+++ app/src/main/java/com/example/manis/recylercardview/adapter/MyAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.manis.recylercardview.R;
@@ -18,13 +19,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
     }
 
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item,parent,false);
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.textView.setText(listItem[position]);
+
     }
 
 
@@ -35,10 +37,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView textView;
+        ImageView imageView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.title);
+            imageView = (ImageView) itemView.findViewById(R.id.img_crd);
         }
     }
 }
