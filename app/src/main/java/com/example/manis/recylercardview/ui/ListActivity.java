61380a47bc0df605f5067826eb6e48827a65ace6
manish <manishmaharzan@gmail.com>
2016-09-25 19:32:27 +0545

    Change the ChildView LinearLayout To Relative Layout insideCardView

--- app/src/main/java/com/example/manis/recylercardview/ui/ListActivity.java
+++ app/src/main/java/com/example/manis/recylercardview/ui/ListActivity.java
@@ -27,7 +27,11 @@ public class ListActivity extends Activity
         recyclerView.setLayoutManager(linearLayoutManager);
 
         //Setting up a hardCoded Data
-        String [] listItem = {"one","two","three","four","5","6","7","8","9","10","11","12","13","14"};
+        String [] listItem =new String[14];
+        for(int i=0;i<14;i++)
+        {
+            listItem[i]=Integer.toString(i);
+        }
 
         adapter = new MyAdapter(listItem);
         recyclerView.setAdapter(adapter);
